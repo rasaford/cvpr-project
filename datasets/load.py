9f8db27ba9906aa2426bf1beb1c8bc52724d0711
purplepers0n <choo.wx96@gmail.com>
2019-10-25 14:56:28 +0800

    Add back load/read samples

--- datasets/load.py
+++ datasets/load.py
@@ -66,6 +66,66 @@ def load_annotations(cat_name: str, dataset_base: str, size=0, n_jobs=4):
 
     return classes
 
+def load_samples(cat_name: str, dataset_base: str, n_jobs=4):
+    """
+    Expects dataset tree of the following shape
+    ├── datasets
+    │   ├── Annotations
+    │   │   └── +.xml
+    │   ├── ImageSets
+    │   │   ├── +.txt
+    │   ├── JPEGImages
+    │   │   ├── +.jpg
+    │   └── load.py
+    """
+    image_set = os.path.join(dataset_base, 'ImageSets/' + cat_name + '.txt')
+    assert os.path.exists(image_set)
+
+    with open(image_set, 'r') as f:
+        files = [l.strip('\n') for l in f.readlines()]
+
+    with mp.Pool(n_jobs) as pool:
+        args = [(dataset_base, file) for file in files]
+        samples = pool.starmap(read_sample, args)
+
+    return samples
+
+def read_sample(dataset_base, file):
+    annotations_path = os.path.join(dataset_base,
+                                    'Annotations/{}.xml'.format(file))
+    img_path = os.path.join(dataset_base, 'JPEGImages/{}.jpg'.format(file))
+
+    sample = dict()
+    root = ET.parse(annotations_path).getroot()
+
+    size = root.find('size')
+    sample['size'] = (int(size.find('width').text),
+                      int(size.find('height').text),
+                      int(size.find('depth').text))
+    sample['filename'] = root.find('filename').text
+    sample['classes'] = []
+    sample['img'] = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+
+    for boxes in root.iter('object'):
+        name = boxes.find('name').text
+        bounds = [[
+            int(box.find('xmin').text),
+            int(box.find('ymin').text),
+            int(box.find('xmax').text),
+            int(box.find('ymax').text)]
+            for box in boxes.findall('bndbox')
+        ]
+
+        # class de-duplication
+        names = [c['name'] for c in sample['classes']]
+        if name in names:
+            sample['classes'][names.index(name)]['bounds'] += bounds
+        else:
+            sample['classes'].append({
+                'name': name,
+                'bounds': bounds
+            })
+    return sample
 
 def _read_sample(dataset_base, file):
     annotations_path = os.path.join(dataset_base, "Annotations/{}.xml".format(file))
